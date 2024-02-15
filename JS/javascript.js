59bb86709e807b3a0c4ccaa5c76d7fac3604d680
Hannah Burwell <hburwell015@gmail.com>
2024-02-15 17:24:14 -0600

    Starting my learning with js

---
+++ JS/javascript.js
@@ -0,0 +1,13 @@
+    console.log("Hello World");
+//in-line coment
+
+/*
+this is a multi line comment*/
+
+var myName = "Hannah" //used through whole code
+
+let ourName ="freeCodeCamp" //only used wwithin scope of where it's written
+
+const pi = 3.14 //variable that should never change
+
+myName = 8
