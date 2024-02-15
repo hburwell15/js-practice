4d0735f36c8975390bfc2a86ca9fcba202d46aca
Hannah Burwell <hburwell015@gmail.com>
2024-02-15 17:34:56 -0600

    variables and how to change them

--- JS/javascript.js
+++ JS/javascript.js
@@ -4,10 +4,18 @@
 /*
 this is a multi line comment*/
 
-var myName = "Hannah" //used through whole code
+var myName = "Hannah"; //used through whole code
 
-let ourName ="freeCodeCamp" //only used wwithin scope of where it's written
+let ourName ="freeCodeCamp"; //only used wwithin scope of where it's written
 
-const pi = 3.14 //variable that should never change
+const pi = 3.14; //variable that should never change
 
-myName = 8
+myName = 8;
+
+var a = 5;
+var b = 10;
+var c = "String";
+
+a = a +1;
+b = b +5;
+c = c + "String!";
