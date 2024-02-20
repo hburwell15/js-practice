d50a0a3c87b392f107f0bedf9bc1ec88247b8c67
Hannah Burwell <hburwell015@gmail.com>
2024-02-19 19:38:40 -0600

    A couple more array exercises and now moving on to functions in js.

---
+++ FreeCodeCamp/manipulateArraysWithShift.js
@@ -0,0 +1,4 @@
+//Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
+
+const myArray = [["John", 23], ["dog", 3]];
+const removedFromMyArray = myArray.shift()
---
+++ FreeCodeCamp/shoppingList.js
@@ -0,0 +1,10 @@
+//Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+/*The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list.*/
+
+
+
+const myList = [["Ice Cream", 1], ["Cheese Snacks", 2], ["Peanut Butter Cups", 8], ["Paper Towels", 20], ["Avocados", 4]];
