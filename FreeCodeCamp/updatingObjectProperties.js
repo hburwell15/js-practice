ebf42f112ec2b248c3a2bbe54f2bafcae59eb4b8
Hannah Burwell <hburwell015@gmail.com>
2024-02-22 21:28:23 -0600

    Learning about using notions in objects to access variables and values

---
+++ FreeCodeCamp/updatingObjectProperties.js
@@ -0,0 +1,11 @@
+/*Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.
+*/
+
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.name = "Happy Coder";
