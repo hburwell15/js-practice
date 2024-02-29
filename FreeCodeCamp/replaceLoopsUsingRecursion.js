f7760d817365c37044c9bf46340d3adc2c1f2b40
Hannah Burwell <hburwell015@gmail.com>
2024-02-28 18:43:29 -0700

    Working with random ints and ranges. Finished with loops and worked with paseInt and radix

---
+++ FreeCodeCamp/replaceLoopsUsingRecursion.js
@@ -0,0 +1,12 @@
+// Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+
+function sum(arr, n) {
+    // Only change code below this line
+    if (n <= 0) {
+        return 1;
+    }
+    else {
+        return sum(arr,)
+    }
+    // Only change code above this line
+  }
